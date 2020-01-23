b9be0c67802b80543fd043cc0714ae4ab2a823b1
Gregor Goldstein <goldsteingregor@gmail.com>
2020-01-23 13:21:11 +0100

    HTML der Site geändert.

---
+++ blog/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+#Hier importieren wir die Django-Funktion path und alle unsere views aus unserer blog-Applikation. (Wir haben noch keine, aber dazu kommen wir gleich!)
+
+#Jetzt können wir unser erstes URL-Muster hinzufügen:
+
+urlpatterns = [
+    path('', views.post_list, name='post_list'),
+]
